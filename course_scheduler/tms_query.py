2c52ef2e762e5ee800b6480c8d58c945d1f96edc
AoWangPhilly <aowangphilly@gmail.com>
2020-12-13 23:39:45 -0500

    set query methods

--- course_scheduler/tms_query.py
+++ course_scheduler/tms_query.py
@@ -5,9 +5,24 @@ import glob
 
 class TMSQuery():
     def __init__(self):
+        
+    def get_professor(self):
+        pass
+
+    def get_credit(self):
+        pass
+
+    def get_no_prereqs(self):
+        pass
+
+    def get_course(self):
+        pass
+
+    def get_crn(self):
         pass
     
 if __name__ == '__main__':
     fall = os.path.join('DREXEL', 'FALL', 'Col of Computing & Informatics', 'CS.csv')
     df = pd.read_csv(fall);
+
     print(df)
