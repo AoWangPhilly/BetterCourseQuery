ed135948d4080fe16aa7d6572fd672fcb59371c2
AoWangPhilly <aowangphilly@gmail.com>
2020-12-13 23:25:14 -0500

    Test out new file

--- course_scheduler/tms_query.py
+++ course_scheduler/tms_query.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
+import os
+import glob
 
 class TMSQuery():
     def __init__(self):
         pass
     
 if __name__ == '__main__':
-    print('Hello')
+    fall = os.path.join('DREXEL', 'FALL', 'Col of Computing & Informatics', 'CS.csv')
+    df = pd.read_csv(fall);
+    print(df)
